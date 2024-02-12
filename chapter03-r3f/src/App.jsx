25a6576398f0d527af0d98b800a2fd2cae2927ba
kojaehoon <kojae0103@naver.com>
2024-02-12 23:25:10 +0900

    r3f Learn

--- chapter03-r3f/src/App.jsx
+++ chapter03-r3f/src/App.jsx
@@ -1,9 +1,11 @@
+import { MainCanvas } from "./components/MainCanvas";
+
 function App() {
   return (
-    <>
-      <h1>React 18 + Vite + Three.js</h1>
-    </>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <MainCanvas />
+    </div>
   )
 }
 
-export default App
+export default App;
